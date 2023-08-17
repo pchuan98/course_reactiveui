3b6d1a9b4fe6010ada332fc49378c4835ea0425d
pchuan98 <haeer98@outlook.com>
2023-08-17 14:11:58 +0800

    note: 使用prism完成基础的映射工作

---
+++ README.md
@@ -0,0 +1,3 @@
+# 上位机测试代码
+
+网课地址为：https://edu.51cto.com/lesson/920456.html
---
+++ Shell/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Prism.Ioc;
+
+namespace Shell
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+             
+        }
+
+        protected override Window CreateShell()
+        {
+            return new Views.MainView();
+        }
+    }
+}
